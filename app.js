3ceaf919d87b9dddf7301ac8e15d1204a7d3e727
6kV <nizarazu9@hotmail.com>
2018-10-24 11:49:20 +0200

    set redux on the project

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
+const { createStore, applyMiddleware } = require('redux');
+
 const defaultState = {
   courses: [
     {
       name: 'Learning React',
-      topic: 'React',
+      topic: 'React'
     },
     {
       name: 'Learning Angular',
-      topic: 'Angular',
+      topic: 'Angular'
     },
     {
       name: 'Using Redux with Angular',
-      topic: 'Angular',
+      topic: 'Angular'
     }
   ]
 };
 
+const logger = store => next => action => {
+    console.log('dispatching ', action);
+    let result = next(action);
+    console.log('state after action ',store.getState)
+    return result;
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'ADD_COURSE' :
+            return Object.assign({},state, {
+                courses: [...state.courses,action.course]
+            });
+    default:
+    return state;
+
+    }
+}
+
+const store = createStore(reducer, defaultState, applyMiddleware(logger));
+
 function addView(viewFunc) {
   viewFunc(defaultState);
+
+    store.subscribe(() => {
+        viewFunc(store.getState());
+    });
 }
 
 addView((state) => {
@@ -27,6 +54,11 @@ addView((state) => {
   console.log(`The latest course in the library: ${state.courses[state.courses.length -1].name}`);
 });
 
-
-
+store.dispatch({
+    type: 'ADD_COURSE',
+    course : {
+        name: 'this is a new course',
+        topic: 'really does not matter'
+    }
+})
 
